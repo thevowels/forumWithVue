5661948d509b351205cc081f902310613a98fad0
thevowels <aungphyohtet1@gmail.com>
2025-03-21 04:18:01 +0700

    Extract thte assertCompoent function to make it as macro to become  reusable on TestingServiceProvider

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -7,5 +7,5 @@ it('can show a post', function () {
     $post = Post::factory()->create();
 
     get(route('posts.show', $post))
-        ->assertInertia(fn (\Inertia\Testing\AssertableInertia $inertia) => $inertia->component('Posts/Show', true));
+        ->assertComponent('Posts/Show', true);
 });
