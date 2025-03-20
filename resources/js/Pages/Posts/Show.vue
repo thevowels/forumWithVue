1e0907f9faac8770f0d4e376f0adb3a5f139af12
thevowels <aungphyohtet1@gmail.com>
2025-03-21 04:15:24 +0700

    Fix -> show.vue on Posts folder 	Fixed

---
+++ resources/js/Pages/Posts/Show.vue
@@ -0,0 +1,11 @@
+<template>
+    <AppLayout>
+        <Container>
+            Show Page
+        </Container>
+    </AppLayout>
+</template>
+<script setup lang="ts">
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Container from "@/Components/Container.vue";
+</script>
